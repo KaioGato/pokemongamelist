efbbf1e47995aaac2dc1d0858878067f423fa1dc
Kaio Gato <kaiomurilo017@gmail.com>
2024-03-19 15:43:34 -0400

    19/03/24

---
+++ scriptList.js
@@ -0,0 +1,126 @@
+ eita
+ function gen1games() {
+           var elementos = document.getElementsByClassName("1genGames");
+        for (var i = 0; i < elementos.length; i++) {
+            var elemento = elementos[i];
+            var estiloDisplay = elemento.style.display;
+
+            if (estiloDisplay === "flex" ) {
+                elemento.style.display = "none"; // Ou qualquer outro valor desejado para exibição
+            } else {
+                elemento.style.display = "flex";
+            }
+        }
+    }
+
+ function gen2games() {
+           var elementos = document.getElementsByClassName("2genGames");
+        for (var i = 0; i < elementos.length; i++) {
+            var elemento = elementos[i];
+            var estiloDisplay = elemento.style.display;
+
+            if (estiloDisplay === "flex" ) {
+                elemento.style.display = "none"; // Ou qualquer outro valor desejado para exibição
+            } else {
+                elemento.style.display = "flex";
+            }
+        }
+    }   
+
+    function gen3games() {
+           var elementos = document.getElementsByClassName("3genGames");
+        for (var i = 0; i < elementos.length; i++) {
+            var elemento = elementos[i];
+            var estiloDisplay = elemento.style.display;
+
+            if (estiloDisplay === "flex" ) {
+                elemento.style.display = "none"; // Ou qualquer outro valor desejado para exibição
+            } else {
+                elemento.style.display = "flex";
+            }
+        }
+    }
+
+ function gen4games() {
+           var elementos = document.getElementsByClassName("4genGames");
+        for (var i = 0; i < elementos.length; i++) {
+            var elemento = elementos[i];
+            var estiloDisplay = elemento.style.display;
+
+            if (estiloDisplay === "flex" ) {
+                elemento.style.display = "none"; // Ou qualquer outro valor desejado para exibição
+            } else {
+                elemento.style.display = "flex";
+            }
+        }
+    }   
+
+     function gen5games() {
+           var elementos = document.getElementsByClassName("5genGames");
+        for (var i = 0; i < elementos.length; i++) {
+            var elemento = elementos[i];
+            var estiloDisplay = elemento.style.display;
+
+            if (estiloDisplay === "flex" ) {
+                elemento.style.display = "none"; // Ou qualquer outro valor desejado para exibição
+            } else {
+                elemento.style.display = "flex";
+            }
+        }
+    }
+
+ function gen6games() {
+           var elementos = document.getElementsByClassName("6genGames");
+        for (var i = 0; i < elementos.length; i++) {
+            var elemento = elementos[i];
+            var estiloDisplay = elemento.style.display;
+
+            if (estiloDisplay === "flex" ) {
+                elemento.style.display = "none"; // Ou qualquer outro valor desejado para exibição
+            } else {
+                elemento.style.display = "flex";
+            }
+        }
+    }   
+
+    function gen7games() {
+           var elementos = document.getElementsByClassName("7genGames");
+        for (var i = 0; i < elementos.length; i++) {
+            var elemento = elementos[i];
+            var estiloDisplay = elemento.style.display;
+
+            if (estiloDisplay === "flex" ) {
+                elemento.style.display = "none"; // Ou qualquer outro valor desejado para exibição
+            } else {
+                elemento.style.display = "flex";
+            }
+        }
+    }
+
+ function gen8games() {
+           var elementos = document.getElementsByClassName("8genGames");
+        for (var i = 0; i < elementos.length; i++) {
+            var elemento = elementos[i];
+            var estiloDisplay = elemento.style.display;
+
+            if (estiloDisplay === "flex" ) {
+                elemento.style.display = "none"; // Ou qualquer outro valor desejado para exibição
+            } else {
+                elemento.style.display = "flex";
+            }
+        }
+    }   
+
+     function gen9games() {
+           var elementos = document.getElementsByClassName("9genGames");
+        for (var i = 0; i < elementos.length; i++) {
+            var elemento = elementos[i];
+            var estiloDisplay = elemento.style.display;
+
+            if (estiloDisplay === "flex" ) {
+                elemento.style.display = "none"; // Ou qualquer outro valor desejado para exibição
+            } else {
+                elemento.style.display = "flex";
+            }
+        }
+    }
